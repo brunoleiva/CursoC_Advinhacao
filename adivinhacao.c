ddaab446db321e263b309e2114b9aa8e9f9f8614
BRUNO LEIVA PIRES <bruno.leivapires2706@gmail.com>
2019-08-09 21:22:34 -0300

    Commit adicionando tentativas

--- adivinhacao.c
+++ adivinhacao.c
@@ -1,20 +1,60 @@
 #include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main()
-{
-	printf("***************************************\n");
-	printf("*Bem vindo ao nosso jogo de advinhacao*\n");
-	printf("***************************************\n");
+void main()
+{ 
 
-	int	numero = 42;	
-	//printf("O numero secreto: %d", numero);
+	printf("***************************\n");
+	printf("*Bem-vindo ao nosso jogo!!*\n");
+	printf("***************************\n");
 
-	int chute;,
+	srand(time(NULL));
+	int numeroSecreto = rand() % 100;
+	int chute;
+	int tentativas = 8;
+	
+	printf("*ADIVINHE O NUMERO DE 1 A 100*\n");
+	//printf("%d\n", chute);
+	while(numeroSecreto != chute && tentativas != 0 ){
 
-	printf("Qual o seu chute? ");
-	scanf("%d", &chute);
-	printf("Seu chute foi %d", chute);
+		printf("Voce tem %d tentativas!\n", tentativas);
+		printf("Qual o seu chute? \n");
+		scanf("%d", &chute);
 
-	printf("VOCE ACERTOU!! BOA!!");
+		if(chute < numeroSecreto){
+			printf("Um pouco MAIOR :o\n\n");
+
+		}
+		else if (chute > numeroSecreto)
+		{
+			printf("Um pouco MENOS :o\n\n");
+		}
+
+		tentativas --;
+
+	}
+
+	if (chute == numeroSecreto)
+	{
+		printf("Parabens, voce ganhou!\n");
+		printf("       ___________      \n");
+		printf("      '._==_==_=_.'     \n");
+		printf("      .-:      /-.    \n");
+		printf("     | (|:.     |) |    \n");
+		printf("      '-|:.     |-'     \n");
+		printf("        ::.    /      \n");
+		printf("         '::. .'        \n");
+		printf("           ) (          \n");
+		printf("         _.' '._        \n");
+		printf("        '-------'       \n");
+	}else if(tentativas == 0){
+		printf("VOCE PERDEU!! >.<\n");
+		printf("O NUMERO ERA: %d \n", numeroSecreto);
+	}
+
+	printf("Aperte alguma tecla para finalizar!\n");
+	getch();
 
 }
