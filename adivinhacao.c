604fc7a6f7c348516fb7f825ffd36336c3a3b4a8
BRUNO LEIVA PIRES <bruno.leivapires2706@gmail.com>
2019-08-12 18:34:48 -0300

    Commit Versao Beta, adicionando jogar novamente!

--- adivinhacao.c
+++ adivinhacao.c
@@ -3,73 +3,76 @@
 #include <stdlib.h>
 #include <time.h>
 
-void main()
-{ 
+int main()
+{ 	
+	int jogar = 1;
 
-	printf("***************************\n");
-	printf("*Bem-vindo ao nosso jogo!!*\n");
-	printf("***************************\n");
+	while(jogar == 1){
+		printf("***************************\n");
+		printf("*Bem-vindo ao nosso jogo!!*\n");
+		printf("***************************\n");
 
-	int dificuldade;
-	int tentativas = 0;
-	printf("(1) Facil \n(2) Medio\n(3) Dificil\n");
-	scanf("%d", &dificuldade);
+		int dificuldade;
+		int tentativas = 0;
+		printf("Escolher dificuldade: \n(1) Facil \n(2) Medio\n(3) Dificil\n");
+		scanf("%d", &dificuldade);
 
-	if(dificuldade ==1){
-		tentativas = 12;
-	}
-	else if(dificuldade ==2){
-		tentativas = 8;
-	}
-	else if(dificuldade ==3){
-		tentativas = 5;
-	}else if (tentativas == 0){
-		printf("*ESCOLHA UMA DIFICULDADE VALIDA >.<*\n");
-	}
+		if(dificuldade ==1){
+			tentativas = 12;
+		}
+		else if(dificuldade ==2){
+			tentativas = 8;
+		}
+		else if(dificuldade ==3){
+			tentativas = 5;
+		}else if (tentativas == 0){
+			printf("*ESCOLHA UMA DIFICULDADE VALIDA >.<*\n");
+		}
 
-	srand(time(NULL));
-	int numeroSecreto = rand() % 100;
-	int chute;
-	
+		srand(time(NULL));
+		int numeroSecreto = rand() % 100;
+		int chute;
+
 	//printf("%d\n", chute);
-	while(numeroSecreto != chute && tentativas != 0 ){
-		printf("*ADIVINHE O NUMERO DE 1 A 100*\n");
-		printf("Voce tem %d tentativas!\n", tentativas);
-		printf("Qual o seu chute? \n");
-		scanf("%d", &chute);
+		while(numeroSecreto != chute && tentativas != 0 ){
+			printf("*ADIVINHE O NUMERO DE 1 A 100*\n");
+			printf("Voce tem %d tentativas!\n", tentativas);
+			printf("Qual o seu chute? \n");
+			scanf("%d", &chute);
 
-		if(chute < numeroSecreto){
-			printf("\n** Um pouco MAIOR :o **\n\n");
+			if(chute < numeroSecreto){
+				printf("\n** Um pouco MAIOR :o **\n\n");
+
+			}
+			else if (chute > numeroSecreto)
+			{
+				printf("\n** Um pouco MENOR :o **\n\n");
+			}
+
+			tentativas --;
 
 		}
-		else if (chute > numeroSecreto)
+
+		if (chute == numeroSecreto)
 		{
-			printf("\n** Um pouco MENOR :o **\n\n");
+			printf("Parabens, voce ganhou!\n");
+			printf("       ___________      \n");
+			printf("      '._==_==_=_.'     \n");
+			printf("      .-:      /-.    \n");
+			printf("     | (|:.     |) |    \n");
+			printf("      '-|:.     |-'     \n");
+			printf("        ::.    /      \n");
+			printf("         '::. .'        \n");
+			printf("           ) (          \n");
+			printf("         _.' '._        \n");
+			printf("        '-------'       \n");
+		}else if(tentativas == 0){
+			printf("VOCE PERDEU!! >.<\n");
+			printf("O NUMERO ERA: %d \n", numeroSecreto);
 		}
 
-		tentativas --;
-
+		printf("\n\n(1) Jogar outra vez!! :D \n(2) SAIR :(\n");
+		scanf("%d", &jogar);
 	}
-
-	if (chute == numeroSecreto)
-	{
-		printf("Parabens, voce ganhou!\n");
-		printf("       ___________      \n");
-		printf("      '._==_==_=_.'     \n");
-		printf("      .-:      /-.    \n");
-		printf("     | (|:.     |) |    \n");
-		printf("      '-|:.     |-'     \n");
-		printf("        ::.    /      \n");
-		printf("         '::. .'        \n");
-		printf("           ) (          \n");
-		printf("         _.' '._        \n");
-		printf("        '-------'       \n");
-	}else if(tentativas == 0){
-		printf("VOCE PERDEU!! >.<\n");
-		printf("O NUMERO ERA: %d \n", numeroSecreto);
-	}
-
-	printf("Aperte alguma tecla para finalizar!\n");
-	getch();
 
 }
